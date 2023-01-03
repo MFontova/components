c1cdd1144a93ae8c5cd368d9c48543fc7504b635
Marc Fontova <mfontovat@gmail.com>
2023-01-03 17:30:39 +0100

    6.67

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -15,10 +15,21 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Listview Tipo 2'),
+          elevation: 0,
+          backgroundColor: Colors.indigoAccent,
         ),
         body: ListView.separated(
-          itemBuilder: (context, index) =>
-              ListTile(title: Text(options[index])),
+          itemBuilder: (context, index) => ListTile(
+            title: Text(options[index]),
+            trailing: const Icon(
+              Icons.keyboard_arrow_right_rounded,
+              color: Colors.indigo,
+            ),
+            onTap: () {
+              final game = options[index];
+              print(game);
+            },
+          ),
           itemCount: options.length,
           separatorBuilder: (_, __) => const Divider(),
         ));
