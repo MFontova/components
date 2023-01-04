c8a15cf1b27a198b137757d8acad3b19278d9aed
Marc Fontova <mfontovat@gmail.com>
2023-01-04 17:15:05 +0100

    6.71

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:components/router/app_routes.dart';
 import 'package:components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
@@ -11,15 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      // home: const Listview2Screen(),
-      initialRoute: 'home',
-      routes: {
-        'home': (context) => const HomeScreen(),
-        'listview1': (context) => const Listview1Screen(),
-        'listview2': (context) => const Listview2Screen(),
-        'card': (context) => const CardScreen(),
-        'alert': (context) => const AlertScreen(),
-      },
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
     );
   }
 }
