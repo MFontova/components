33a9c251b2d817b6de9a2b3d5f884b9639542acf
Marc Fontova <mfontovat@gmail.com>
2023-01-07 22:20:00 +0100

    6.96

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,38 +10,65 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnable = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sliders & Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 0,
-              max: 500,
-              activeColor: AppTheme.primary,
-              value: _sliderValue,
-              onChanged: (value) {
-                print(value);
-                _sliderValue = value;
-                setState(() {});
-              },
-            ),
-            Image(
-              image: NetworkImage(
-                'https://static.wikia.nocookie.net/killbill/images/2/22/Elle_Driver.jpg',
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 0,
+            max: 500,
+            activeColor: AppTheme.primary,
+            value: _sliderValue,
+            onChanged: _sliderEnable
+                ? (value) {
+                    print(value);
+                    _sliderValue = value;
+                    setState(() {});
+                  }
+                : null,
+          ),
+          Checkbox(
+            value: _sliderEnable,
+            onChanged: (value) {
+              _sliderEnable = value ?? true;
+              setState(() {});
+            },
+          ),
+          CheckboxListTile(
+            activeColor: AppTheme.primary,
+            title: Text('Habilitar Slider'),
+            value: _sliderEnable,
+            onChanged: (value) {
+              _sliderEnable = value ?? true;
+              setState(() {});
+            },
+          ),
+          SwitchListTile(
+            title: const Text('Habilitar slider'),
+            activeColor: AppTheme.primary,
+            value: _sliderEnable,
+            onChanged: (value) {
+              _sliderEnable = value;
+              setState(() {});
+            },
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage(
+                  'https://static.wikia.nocookie.net/killbill/images/2/22/Elle_Driver.jpg',
+                ),
+                fit: BoxFit.fitWidth,
+                width: _sliderValue,
               ),
-              fit: BoxFit.fitWidth,
-              width: _sliderValue,
             ),
-            SizedBox(
-              height: 100,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
